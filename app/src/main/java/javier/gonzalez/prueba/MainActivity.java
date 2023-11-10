de37179a13047e18933a1245687ba9eecc33a301
Javier2992 <123154776+Javier2992@users.noreply.github.com>
2023-11-10 20:10:16 +0100

    ñ

--- app/src/main/java/javier/gonzalez/prueba/MainActivity.java
+++ app/src/main/java/javier/gonzalez/prueba/MainActivity.java
@@ -1,5 +1,9 @@
 package javier.gonzalez.prueba;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -8,7 +12,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import javier.gonzalez.prueba.modelos.Direccion;
 import javier.gonzalez.prueba.modelos.Usuario;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,13 +24,15 @@ public class MainActivity extends AppCompatActivity {
     private EditText txtEmail;
 
     private Button btnCrearDir;
+    private ActivityResultLauncher<Intent> launcherDirecciones;
 
-    private final int DIRECCIONES = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         inicializarVariables();
+        inicializarLaunchers();
 
 
         btnDesencriptar.setOnClickListener(new View.OnClickListener() {
@@ -47,12 +55,39 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,
                         CrearDireccionActivity.class);
-                startActivityForResult(intent, DIRECCIONES);
+                launcherDirecciones.launch(intent);
+
+
             }
         });
 
 
+    }
+    private void inicializarLaunchers() {
 
+        launcherDirecciones = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result)
+                    {
+                        if(result.getResultCode() == RESULT_OK){
+                            if(result.getData() != null){
+                                Bundle bundle =
+                                        result.getData().getExtras();
+                                Direccion dir = (Direccion)
+                                        bundle.getSerializable("DIR");
+                                Toast.makeText(MainActivity.this,
+                                        dir.toString(), Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(MainActivity.this, "No hay datos", Toast.LENGTH_SHORT).show();
+                            }
+                        }else{
+                            Toast.makeText(MainActivity.this, "Ventana Cancelada", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
     }
 
     private void inicializarVariables() {
@@ -62,3 +97,4 @@ public class MainActivity extends AppCompatActivity {
         btnCrearDir = findViewById(R.id.btnDireccionCrear);
     }
 }
+
